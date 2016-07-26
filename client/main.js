8420679d0c4ddcc55025296804c7e2e13919fca9
amansawhney <amansawhney@sawhneygames.com>
2016-07-26 00:36:33 -0400

    Changed App componet from a function based componet to a class based componet

--- client/main.js
+++ client/main.js
@@ -1,20 +1,25 @@
 //Any JS in here is automatically ran for us
 
 // Import the React lib
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
+import axios from 'axios'
 // Create a component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 
 // Render the component to the sceen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+    .then(response => console.log(response));
 });
